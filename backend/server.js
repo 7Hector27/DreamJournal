a00aaaf2040bed8c61c880b1753e5000d1687082
hector27 <hector27c7@gmail.com>
2021-11-18 10:27:23 -0800

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 const bp = require('body-parser');
 
@@ -19,6 +20,14 @@ app.use(bp.urlencoded({ extended: true }));
 
 app.use('/api', userRoutes, journalRoutes);
 connectDb();
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`);
